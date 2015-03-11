661b022b8fb63e0f253d0ea80939ec91725ee6b7
Nils Blum-Oeste <nblumoeste@googlemail.com>
2015-03-11 12:59:54 +0100

    Remove test build artifact from repo

--- test/pva_parrot/backend/service_test.clj
+++
@@ -1,8 +0,0 @@
-(ns pva-parrot.backend.service-test
-  (:require [clojure.test :refer :all]
-            [pva-parrot.backend.service :refer :all]))
-
-(deftest backend-service
-  (testing "Event Handling"
-    (testing "file import"
-      (is (= nil (event-msg-handler {:id :pva-parrot/import-file}))))))
